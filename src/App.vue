1f859ceaea80a372d9d92a808d246510b72175ce
julian-collazos <julianchis450@hotmail.com>
2023-10-04 21:14:17 -0500

    Dividiendo la pantalla en dos columnas y agregando una tabla con bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {productos} from './components/datos.js'
 import {ref, computed} from "vue"
+import Header from '@/components/Header.vue'
 const contador = ref(0)
 const ruta="https://www.html6.es/img/rey_"
 const incremento=()=>{
@@ -21,33 +22,83 @@ const imgn=computed(()=>{
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <center><h1>Cenas</h1></center>
-    </div>
-  </header>
-  <body>
+  <div>
+    <Header></Header>
     <!-- 
     <h1> cena con el rey {{contador + 1}} {{nombreRey}}</h1>
     <br>
     <img :src=imgn alt=""/>
     <button class="btn" @click="incremento()">Siguiente {{contador + 1}}/7</button>
     -->
-    <div class="container">
-      <table class="table">
-        <tr>
-          <th>Nombre</th>
-          <th>Precio</th>
-          <th>Fin de semana</th>
-        </tr>
-        <tr v-for="rey in productos">
-          <td v-text="rey.nombre"></td>
-          <td v-text="rey.precio"></td>
-          <td v-text="rey.finDeSemana"></td>
-        </tr>
-      </table>
+    <div class="container text-center">
+      <div class="row justify-content-between">
+        <div class="col">
+          <div class="row">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Precio</th>
+                  <th scope="col">Fin de semana</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="rey in productos">
+                  <td v-text="rey.nombre"></td>
+                  <td v-text="rey.precio"></td>
+                  <td v-text="rey.finDeSemana"></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="row">
+            <button class="btn btn-success">Prueba de boton</button>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Accordion Item #1
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Accordion Item #2
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Accordion Item #3
+                </button>
+              </h2>
+              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </body>
+  </div>
 
 </template>
 
@@ -55,7 +106,7 @@ const imgn=computed(()=>{
 header {
   line-height: 1.5;
 }
-.btn{
+.btns{
   display: flex;
   place-items: center;
   padding: 1em 2em;
