22b626e9edb89dc460482775bddc84edcd7b6e71
julian-collazos <julianchis450@hotmail.com>
2023-10-04 21:15:53 -0500

    Agregando el header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav class="navbar fixed-top" style="background-color: #00940c;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <img src="../assets/Logo_HummingWings.png" alt="Logo" width="60" height="50" class="d-inline-block align-text-top">
+        Bootstrap
+        </a>
+      </div>
+    </nav>
+    <br><br><br><br>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
